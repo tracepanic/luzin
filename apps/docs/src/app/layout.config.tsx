d4b1592daa3c8f9eb6775fdd021133831cabd180
tracepanic <patrickscript@proton.me>
2025-05-11 13:22:11 +0300

    ``` Update documentation structure and layout configurations
    - Added admin documentation with metadata and introduction page.
    - Updated developer documentation content and metadata.
    - Removed unused test documentation file.
    - Simplified layout configuration by removing unused links and banners.
    - Enhanced source loader to support dynamic icons.
    ```

--- apps/docs/src/app/layout.config.tsx
+++ apps/docs/src/app/layout.config.tsx
@@ -1,7 +1,4 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
-import { Book, UserCog, UserPen, Users } from "lucide-react";
-import Image from "next/image";
-import Banner from "public/banner.svg";
 
 /**
  * Shared layout configurations
@@ -27,71 +24,4 @@ export const baseOptions: BaseLayoutProps = {
       </>
     ),
   },
-  links: [
-    {
-      type: "menu",
-      text: "Documentation",
-      url: "/docs/devs",
-      items: [
-        {
-          menu: {
-            banner: (
-              <div className="-mx-3 -mt-3">
-                <Image
-                  src={Banner}
-                  alt="Banner"
-                  className="rounded-t-lg object-cover p-5 h-56"
-                  style={{
-                    maskImage:
-                      "linear-gradient(to bottom,white 60%,transparent)",
-                  }}
-                />
-              </div>
-            ),
-            className: "md:row-span-2",
-          },
-          icon: <Book />,
-          text: "Getting Started",
-          description: "Learn how to setup Luzin learning managment system.",
-          url: "/docs/devs",
-        },
-        {
-          icon: <UserCog />,
-          text: "Administrators",
-          description: "Learn how to manage Luzin LMS as an admin.",
-          url: "/docs/admins",
-          menu: {
-            className: "lg:col-start-2",
-          },
-        },
-        {
-          icon: <UserPen />,
-          text: "Teachers",
-          description: "Learn how to use Luzin LMS as a teacher.",
-          url: "/docs/teachers",
-          menu: {
-            className: "lg:col-start-3 lg:row-start-1",
-          },
-        },
-        {
-          icon: <Users />,
-          text: "Parents",
-          description: "Learn how to use Luzin LMS as a parent.",
-          url: "/docs/parents",
-          menu: {
-            className: "lg:col-start-2",
-          },
-        },
-        {
-          icon: <UserCog />,
-          text: "Student",
-          description: "Learn how to use Luzin LMS as a student.",
-          url: "/docs/student",
-          menu: {
-            className: "lg:col-start-3",
-          },
-        },
-      ],
-    },
-  ],
 };
